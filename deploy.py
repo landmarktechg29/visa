e2081e1cd265ec617799fc4e89152715d9d6e0ae
George Akagbor <akagborg@gmail.com>
2022-08-20 04:24:50 -0400

    version2

--- deploy.py
+++ deploy.py
@@ -1,5 +1,7 @@
 # Deploying a python web application
-echo "Hello World"
+# echo "Hello World"
 #= shell script
-python('Hello World')
+# python('Hello World')
 #= python script
+print('Hello World')
+
